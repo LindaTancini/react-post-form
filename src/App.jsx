d7a48763c75ad5868632c4b2446f54a80eef9526
LindaTancini <lindatancini95@gmail.com>
2025-04-14 14:25:36 +0200

    feat: create posts

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import { useState } from "react";
 import axios from "axios";
 
 function App() {
-  return <></>;
+  //CREO I CAMPI CHE ANDRANNO INSERITI NEL FORM
+  const [formPost, setFormPost] = useState({
+    author: "",
+    title: "",
+    body: "",
+    public: false,
+  });
 }
 
 export default App;
